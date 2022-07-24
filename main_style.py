faa54145c004d7936008011a3d9dee7777dc9050
RadientBrain <54241703+RadientBrain@users.noreply.github.com>
2022-07-24 20:51:13 +0530

    Update main_style.py

--- main_style.py
+++ main_style.py
@@ -17,7 +17,7 @@ def load_image(image_path, image_size=(512, 256)):
     """Loads and preprocesses images."""
     # Cache image file locally.
     if "http" in image_path:
-        image_path = tf.keras.utils.get_file(os.path.basename(image_url)[-128:], image_path)
+        image_path = tf.keras.utils.get_file(os.path.basename(image_path)[-128:], image_path)
     # Load and convert to float32 numpy array, add batch dimension, and normalize to range [0, 1].
     img = tf.io.decode_image(
       tf.io.read_file(image_path),
